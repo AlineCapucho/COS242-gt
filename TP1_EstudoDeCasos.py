03a07731b637fb076baf50d7b4e19bfc0f3d947e
ananiasmachado <ananiasmachado143@gmail.com>
2021-12-23 18:20:59 -0300

    Atualizado estudo de casos

--- TP1_EstudoDeCasos.py
+++ TP1_EstudoDeCasos.py
@@ -1,6 +1,10 @@
 from TP1 import Graph, Vertice
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-# """ Estudo de Casos Pergunta 1 """
+""" Estudo de Casos Pergunta 1 """
 # mygraph = Graph()
 # mygraph.create_from_file('grafo_4.txt', kind='matrix')
 
@@ -11,22 +15,96 @@ from TP1 import Graph, Vertice
 
 """ Estudo de Casos Pergunta 2 """
 # mygraph = Graph()
-# mygraph.create_from_file('grafo_1.txt', kind='list')
+# mygraph.create_from_file('grafo_5.txt', kind='list')
 
 # Os tempos de execução das bfs foram calculados para diferentes vértices e armazenados em um txt
 # A forma como fizemos isso está exemplificada no código a seguir. No caso de grafos com pelo
 # menos 1000 vértices, não foi necessário calcular o mod do número do vértice que a bfs será
 # realizada.
-# with open('bfstimersgrafo1.txt', 'a') as f:
+# with open('bfstimersgrafo5matrix.txt', 'a') as f:
 #     for i in range(1, 1000+1):
-#         v = i % 100
-#         if (v == 0):
-#             v = 100
+#         # A linha abaixo é para quando o grafo tem pelo menos 1000 vértices
+#         v = i
+#         # As três linhas abaixo são para quando o grafo tem menos de 1000 vértices
+#         # v = i % 100
+#         # if (v == 0):
+#         #     v = 100
 #         startTime = time.time()
 #         mygraph.bfs(v)
 #         executionTime = time.time() - startTime
 #         print("Tempo de execução da bfs: {} segundos".format(str(executionTime)))
 #         f.write('{} {}\n'.format(v, executionTime))
+
+""" Estudo de Casos Pergunta 3 """
+# mygraph = Graph()
+# mygraph.create_from_file('grafo_1.txt', kind='list')
+
+# Os tempos de execução das dfs foram calculados para diferentes vértices e armazenados em um txt
+# A forma como fizemos isso está exemplificada no código a seguir. No caso de grafos com pelo
+# menos 1000 vértices, não foi necessário calcular o mod do número do vértice que a dfs será
+# realizada.
+# with open('dfstimersgrafo1.txt', 'a') as f:
+#     for i in range(1, 1000+1):
+#         # A linha abaixo é para quando o grafo tem pelo menos 1000 vértices
+#         v = i
+#         # As três linhas abaixo são para quando o grafo tem menos de 1000 vértices
+#         # v = i % 100
+#         # if (v == 0):
+#         #     v = 100
+#         startTime = time.time()
+#         mygraph.dfs(v)
+#         executionTime = time.time() - startTime
+#         print("Tempo de execução da dfs: {} segundos".format(str(executionTime)))
+#         f.write('{} {}\n'.format(v, executionTime))
+
+""" Estudo de Casos Pergunta 4 """
+# Para cada grafo foi utilizado este mesmo código, mudando apenas o grafo que está sendo lido.
+# mygraph = Graph()
+# mygraph.create_from_file('grafo_2.txt', kind='list')
+# for v in [1, 2, 3]:
+#     mygraph.dfs(v)
+#     with open('dfs.txt', 'r') as f:
+#         for i in range(7):
+#             f.readline()
+#         for u in [10, 20, 30]:
+#             for i in range(10):
+#                 line = f.readline().split(' ')
+#             pai = line[1]
+#             print(f"O pai do vértice {u} quando a bfs é realizada no vértice {v} é o "+\
+#                 "vértice {pai}.")
+
+""" Estudo de Casos Pergunta 5 """
+# Para cada grafo foi utilizado este mesmo código, mudando apenas o grafo que está sendo lido.
+# mygraph = Graph()
+# mygraph.create_from_file('grafo_2.txt', kind='list')
+# mygraph.distancia(10, 20)
+# mygraph.distancia(10, 30)
+# mygraph.distancia(20, 30)
+
+""" Estudo de Casos Pergunta 6 """
+# Para cada grafo foi utilizado este mesmo código, mudando apenas o grafo que está sendo lido.
+# mygraph = Graph()
+# mygraph.create_from_file('grafo_2.txt', kind='list')
+# conexos_list = mygraph.conexos()
+# conexos_dict = {i:conexos_list.count(i) for i in set(conexos_list)}
+# biggest_connected = max(conexos_dict, key=conexos_dict.get)
+# smallest_connected = min(conexos_dict, key=conexos_dict.get)
+# length = len(set(conexos_list))
+# print(f"O grafo possui {length} componentes conexas.")
+# print(f"A maior componentes conexa possui {conexos_dict[biggest_connected]} vértices.")
+# print(f"A menor componentes conexa possui {conexos_dict[smallest_connected]} vértices.")
+
+""" Estudo de Casos Pergunta 7 """
+# mygraph = Graph()
+# mygraph.create_from_file("grafo_2.txt", kind='list')
+# mygraph.diametro()
+
+""" Calculando a média """
+# O código a seguir pode ser facilmente modificado para calcular o tempo médio de uma busca
+# para diferentes arquivos de tempo.
+# df0 = pd.read_csv('bfstimersgrafo5matrix.txt', sep=' ')
+# mean = df0['time'].mean()
+# print(f"O tempo médio de uma BFS é {mean} segundos")
 
 """ Gerando os gráficos """
 
@@ -34,24 +112,19 @@ from TP1 import Graph, Vertice
 
 # df0 = pd.read_csv('bfstimersgrafo6.txt', sep=' ')
 
-# df0['time'] = df0['time'] * 1000
+# # df0['time'] = df0['time'] * 1000
 
-# # ax.bar(df0['vertice'], df0['time'])
-# ax.hist(df0['time'], bins=30)
+# ax.scatter(df0['vertice'], df0['time'], c=df0.index)
+# # ax.hist(df0['time'], bins=30)
 
 # ax.set_title('Time required for the BFS in each vertice')
 # ax.set_xlabel('Vertice')
-# ax.set_ylabel('Time required (ms)')
+# ax.set_ylabel('Time required (s)')
 # ax.tick_params(which='minor', width=0.75, length=2.5)
 # # ax.set_xlim(0, 1000)
 # # ax.set_yticks(np.arange(0, 7, 0.5))
 # fig.set_size_inches([8, 6])
 
 # plt.show()
-
-""" Estudo de Casos Pergunta 7 """
-# mygraph = Graph()
-# mygraph.create_from_file("grafo_2.txt", kind='list')
-# mygraph.diametro()
 
 ### TAIL ###
